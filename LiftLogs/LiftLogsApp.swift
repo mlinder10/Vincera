3dc4d3fe9c024be556cae4f8633df4aa7a45e7d4
Matt Linder <linder2015@outlook.com>
2024-10-21 17:44:08 -0400

    Initial Commit

---
+++ LiftLogs/LiftLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogsApp.swift
+//  LiftLogs
+//
+//  Created by Matt Linder on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
